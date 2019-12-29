a20dc5858f9f7ea9dfbaf5d012e9cc9187ce991a
larfoley <lar_x87@yahoo.com>
2019-12-29 14:05:04 +0000

    fix tests and deprecations warnings

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,6 +4,7 @@ import DataAdapterMixin from "ember-simple-auth/mixins/data-adapter-mixin";
 
 export default RESTAdapter.extend(DataAdapterMixin, {
   host: 'http://localhost:3000',
+
   headers: computed('session.data.authenticated.token', function() {
     const headers = {};
     if (this.session.isAuthenticated) {
@@ -11,5 +12,27 @@ export default RESTAdapter.extend(DataAdapterMixin, {
     }
 
     return headers;
-  })
+  }),
 });
+
+// import Controller from '@ember/controller';
+// import { action } from '@ember/object'
+// import { inject as service } from '@ember/service';
+// import { tracked } from '@glimmer/tracking';
+// import { computed } from '@ember/object';
+//
+// export default class ApplicationAdapter extends RESTAdapter.extend(DataAdapterMixin, {}) {
+//   host = 'http://localhost:3000';
+//   sendClientIdAsQueryParam = true;
+//
+//   @action
+//   @computed('session.data.authenticated.token')
+//   get headers() {
+//     const headers = {};
+//     if (this.session.isAuthenticated) {
+//       headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
+//     }
+//
+//     return headers;
+//   }
+// }

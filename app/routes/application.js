7cfa265db48e0d06fd80aa687b26fd3f083795db
Laurence <larfoley@yahoo.com>
2020-07-29 22:29:16 +0100

    configure socket for host for prod

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 import { inject as service } from '@ember/service';
+import ENV from '../config/environment';
+
 const { host } = ENV.APP;
 
 export default Route.extend(ApplicationRouteMixin, {
